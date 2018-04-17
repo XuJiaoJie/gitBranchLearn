f57931e953f550d9f612f16db093d3a16985ee5f
xujiaojie <912091618@qq.com>
2018-04-17 11:08:23 +0800

    第二分支新增类

---
+++ app/src/main/java/com/netease/nanny/gitbranchlearn/SecondAdd.java
@@ -0,0 +1,12 @@
+package com.netease.nanny.gitbranchlearn;
+
+/**
+ * Created by xujiaojie on 2018/4/17.
+ */
+
+public class SecondAdd {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
