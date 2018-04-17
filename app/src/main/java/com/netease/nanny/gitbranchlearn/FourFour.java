85f84c4cc4f160628c85f9306cb87ab2e164f960
xujiaojie <912091618@qq.com>
2018-04-17 11:45:07 +0800

    第二分支合并后新增

---
+++ app/src/main/java/com/netease/nanny/gitbranchlearn/FourFour.java
@@ -0,0 +1,12 @@
+package com.netease.nanny.gitbranchlearn;
+
+/**
+ * Created by xujiaojie on 2018/4/17.
+ */
+
+public class FourFour {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
