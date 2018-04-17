50141fa4848bd5b8cb9bc69df47d66140108cebf
xujiaojie <912091618@qq.com>
2018-04-17 12:00:33 +0800

    第四分支创造冲突

--- app/src/main/java/com/netease/nanny/gitbranchlearn/MainActivity.java
+++ app/src/main/java/com/netease/nanny/gitbranchlearn/MainActivity.java
@@ -3,11 +3,15 @@ package com.netease.nanny.gitbranchlearn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinkedList linkedList = new LinkedList();
     }
 }
